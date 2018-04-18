7f5ca9f1e4468116c9a66eed4756ae8cc4f86870
Shashank Shukla <shuklashashank@users.noreply.github.com>
2018-04-18 21:51:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='DjangoFileStorageHandler',
       version='1.0',
@@ -6,5 +7,6 @@ setup(name='DjangoFileStorageHandler',
       author='Shashank Shukla',
       author_email='shuklashashank@outlook.com',
       url='http://pypi.python.org/pypi/DjangoFileStorageHandler',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
       install_requires =['minio>=3.0.4', 'requests>=2.18.2', 'boto3>=1.5.19', 'Django>=1.11'],
      )
